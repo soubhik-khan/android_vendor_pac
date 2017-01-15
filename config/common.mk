tudourom/android_vendor_crdroid

vendor: Install Adaway as system app
Change-Id: Id6587a2fda70af42f4a78b8adf47afa2980bb365
Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

config/common.mk
@@ -241,6 +241,10 @@ DEVICE_PACKAGE_OVERLAYS += vendor/cm/overlay/common
 # Don't compile SystemUITests
 EXCLUDE_SYSTEMUI_TESTS := true
 
+# Adaway
+PRODUCT_COPY_FILES += \
+    vendor/cm/prebuilt/Adaway/Adaway.apk:system/app/Adaway/Adaway.apk
+
 # Product version should match Android version
 PRODUCT_VERSION_MAJOR = 7
 PRODUCT_VERSION_MINOR = 1

soubhik-khan/android_vendor_pac

Conflicts:
	config/common.mk

config/common.mk
@@ -227,6 +227,10 @@ endif
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.sys.root_access=1
 
+# Adaway
+PRODUCT_COPY_FILES += \
+    vendor/pac/prebuilt/Adaway/Adaway.apk:system/app/Adaway/Adaway.apk
+
 -include $(WORKSPACE)/build_env/image-auto-bits.mk
 
 $(call prepend-product-if-exists, vendor/extra/product.mk)